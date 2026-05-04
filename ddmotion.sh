faaa6261e701a561a947f0c3ed5e05fd5a797899
2vibeornot2vibe <gjason2024@protonmail.com>
2026-05-04 22:09:26 +0800

    Performance optimization

--- ddmotion.sh
+++ ddmotion.sh
@@ -25,6 +25,9 @@ then
     exit
 fi
 
+# Get terminal window width
+COLS=$(tput cols)
+
 # List block devices with some additional info
 lsblk --output NAME,MODEL,SERIAL,SIZE
 echo
@@ -49,50 +52,47 @@ do
     echo -e "\e[31mERROR: Please enter a non‑negative integer!\e[0m"
 done
 
-# Get device's parameters
+# Get device's sector size
 SECTOR_SIZE=$(blockdev --getss "$DEV")
-TOTAL_SECTORS=$(expr $(blockdev --getsize64 "$DEV") / $SECTOR_SIZE)
-
+# Calculate device's total sector count
+TOTAL_SECTORS=$(($(blockdev --getsize64 "$DEV")/SECTOR_SIZE))
 # Calculate the LBA range for perturbations
 MAX_OFFSET=$(awk -v ts="$TOTAL_SECTORS" -v pr="$PERT_RANGE" 'BEGIN {printf "%.0f", ts*pr}')
 
-# Use dd to seek to a specified offset, but apply some "perturbation" as a work-around of read caching on some devices
-# Then print a # in the terminal to reflect the current seek position
-exec_seek() {
-    local skip_lba
-    # Get the new "perturbed" LBA offset
-    skip_lba=$(awk -v p="$1" -v t="$TOTAL_SECTORS" -v m="$MAX_OFFSET" -v s="$RANDOM" '
-        BEGIN {
-            srand(s)
-            lba=int(p*(t-1))
-            offset=int(rand()*(2*m+1))-m
-            skip=lba+offset
-            if (skip<0) skip=0
-            if (skip>=t) skip=t-1
-            printf "%d", skip
-        }')
-    # Read a single block matching device's sector size using dd
-    # Also bypass OS's page cache, continue on errors, hide all outputs and error messages
-    dd if="$DEV" iflag=direct skip="$skip_lba" of=/dev/null bs="$SECTOR_SIZE" count=1 conv=noerror status=none 2>/dev/null
-    # Print a #, dynamically adapting to the current terminal width
-    printf "%$(($(awk -v p="$1" -v w=$(tput cols) 'BEGIN {printf "%.0f", p*(w-1)}')))s#\n"""
-}
-
-export -f exec_seek
-
-# Generate the original patterns from hdmotion.cpp and execute seeks
 run_patt() {
-    awk '
+    awk -v dev="$DEV" -v ss="$SECTOR_SIZE" -v ts="$TOTAL_SECTORS" -v mo="$MAX_OFFSET" -v cols="$COLS" '
+    # Use dd to seek to a specified offset, but apply some "perturbation" as a work-around of read caching on some devices
+    # Then print a # in the terminal to reflect the current seek position
+    function exec_seek(p) {
+        # Get the new "perturbed" LBA offset
+        lba=int(p*(ts-1))
+        offset=int(rand()*(2*mo+1))-mo
+        skip=lba+offset
+        if (skip<0) skip=0
+        if (skip>=ts) skip=ts-1
+
+        # Read a single block matching the sector size using dd
+        # Also bypass page cache, continue on errors, hide all outputs and error messages
+        cmd="dd if=" dev " iflag=direct skip=" skip " of=/dev/null bs=" ss " count=1 conv=noerror status=none 2>/dev/null"
+        system(cmd)
+
+        # Print a #, adapted to the current terminal width
+        w=int(p*(cols-1))
+        printf "%*s#\n", w, ""
+    }
+
+    # Generate the original patterns from hdmotion.cpp and execute seeks
     BEGIN {
+        srand()
         pi=3.141592653589793
 
         # accelerating zigzag
         s=0.01
         f=0.0
         for (i=0; i<5; i++) {
-            for (; f<1.0; f+=s) print f
+            for (; f<1.0; f+=s) exec_seek(f)
             f-=s
-            for (; f>0.0; f-=s) print f
+            for (; f>0.0; f-=s) exec_seek(f)
             s+=0.0075
         }
         f+=s
@@ -101,8 +101,8 @@ run_patt() {
         h=0.90
         l=0.10
         while (l<h) {
-            for (; f<h; f+=s) print f
-            for (; f>l; f-=s) print f
+            for (; f<h; f+=s) exec_seek(f)
+            for (; f>l; f-=s) exec_seek(f)
             h-=0.05
             l+=0.05
         }
@@ -110,21 +110,21 @@ run_patt() {
         # widening sinusoid
         for (amp=0.05; amp<=0.50; amp+=0.05) {
             for (x=0; x<2*pi; x+=pi/32)
-                print sin(x)*amp+0.5
+                exec_seek(sin(x)*amp+0.5)
         }
 
         # narrowing sinusoid
         for (amp=0.50; amp>0.0; amp-=0.05) {
             for (x=0; x<2*pi; x+=pi/32)
-                print sin(x)*amp+0.5
+                exec_seek(sin(x)*amp+0.5)
         }
 
         # widening double-sinusoid
         for (amp=0.05; amp<=0.50; amp+=0.05) {
             for (x=0; x<2*pi; x+=pi/16) {
                 f=sin(x)*amp+0.5
-                print f
-                print 1.0-f
+                exec_seek(f)
+                exec_seek(1.0-f)
             }
         }
 
@@ -132,8 +132,8 @@ run_patt() {
         for (amp=0.50; amp>0.0; amp-=0.05) {
             for (x=0; x<2*pi; x+=pi/16) {
                 f=sin(x)*amp+0.5
-                print f
-                print 1.0-f
+                exec_seek(f)
+                exec_seek(1.0-f)
             }
         }
 
@@ -143,29 +143,31 @@ run_patt() {
             repeat=int(160/heads)
             for (i=0; i<repeat; i++)
                 for (j=1; j<=heads; j++)
-                    print j/(heads+1)
+                    exec_seek(j/(heads+1))
             heads++
         }
         while (heads>0) {
             repeat=int(160/heads)
             for (i=0; i<repeat; i++)
                 for (j=1; j<=heads; j++)
-                    print j/(heads+1)
+                    exec_seek(j/(heads+1))
             heads-=2
         }
 
         # noise
         for (i=0; i<600; i++)
-            print rand()
-    }' |
-    while read -r p
-    do
-        exec_seek "$p"
-    done
+            exec_seek(rand())
+    }'
 }
+
+# Trap the process so it terminates when Ctrl+C is pressed
+trap 'kill "$PID" 2>/dev/null; echo -e "\n\e[31mInterrupted! Exiting...\e[0m"; exit 0' INT
 
 # Main loop
 for ((i=0; LOOPS==0 || i<LOOPS; i++))
 do
-    run_patt
+    run_patt &
+    PID=$!
+    wait "$PID"
+    PID=""
 done
